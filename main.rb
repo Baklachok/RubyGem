8cfaf651f4741bf809aea0222aa6eae75ba0721d
Даниил Коробко <korobko_01@inbox.ru>
2024-04-06 19:47:03 +0300

    refactored code

--- main.rb
+++ main.rb
@@ -1,9 +1,6 @@
+require 'docx'
 
-    require 'docx'
-
-
-
-gg = {
+replacements = {
     'Факультет':  '$1',
     'Название работы':  '$2',
     '№ курса':  '№ курса',
@@ -17,51 +14,33 @@ gg = {
     'Город': 'Город',
     'Год': 'Год',
 }
+
 doc = Docx::Document.open('./template.docx')
 
-readVariable = Docx::Document.open('./forVariable.docx')
-readVariable.paragraphs.each do |readStr|
+read_variable_doc = Docx::Document.open('./forVariable.docx')
 
-    # puts readStr.to_s.split("-")
-    # if(readStr.to_s ) сделать проверку
-    replacement_value = readStr.to_s.split("–")
-    puts "asdf " + gg[replacement_value[0].strip.to_sym].to_s + " /" + replacement_value[0].strip + "/"
-    replacement_value[0] = gg[replacement_value[0].strip.to_sym].to_s
-    replacement_value[1] = replacement_value[1].strip
-    # puts "/" + replacement_value[0] + "/"
-    # puts replacement_value[1]
-    doc.paragraphs.each do |p|
+read_variable_doc.paragraphs.each do |read_str|
+  replacement_value = read_str.to_s.split("–")
+  replacement_key = replacement_value[0].strip.to_sym
+  replacement_value[0] = replacements[replacement_key].to_s
+  replacement_value[1] = replacement_value[1].strip
 
-        p.each_text_run do |tr|
-
-            tr.substitute(replacement_value[0], replacement_value[1])      
-            # puts tr.to_s + " replace on  " + replacement_value[0]
-        end
-        # puts replacement_value[0]
-        # p.text = p.text.sub(replacement_value[0], replacement_value[1])
-        
-        # p.styles.font = 'Times New Roman'
-        # Создаем стиль с нужным шрифтом и размером текста
-        # doc.default_paragraph_style
-        # puts "replace " + replacement_value[0] + "  p " +   p
-
+  doc.paragraphs.each do |p|
+    p.each_text_run do |tr|
+      tr.substitute(replacement_value[0], replacement_value[1])
     end
+  end
 
-    doc.tables.each do |table|
-        last_row = table.rows.last
-      
-        # Substitute text in each cell of this new row
-        last_row .cells.each do |cell|
-          cell.paragraphs.each do |paragraph|
-            paragraph.each_text_run do |text|
-                # puts text
-              text.substitute(replacement_value[0], replacement_value[1])
-            end
-          end
+ doc.tables.each do |table|
+    last_row = table.rows.last
+    last_row.cells.each do |cell|
+      cell.paragraphs.each do |paragraph|
+        paragraph.each_text_run do |text|
+          text.substitute(replacement_value[0], replacement_value[1])
         end
       end
-end  
-
-
+    end
+  end
+end
 
 doc.save('example.docx')
