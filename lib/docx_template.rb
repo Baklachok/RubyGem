22d389203730451b6b0117227b8ec1b95d5978d8
Даниил Коробко <korobko_01@inbox.ru>
2024-04-07 13:56:35 +0300

    added class for course work

--- lib/docx_template.rb
+++ lib/docx_template.rb
@@ -6,6 +6,74 @@ require "docx"
 module DocxTemplate
   class Error < StandardError; end
 
+  class Replacements
+    attr_accessor :faculty, :work_title, :course_number, :group_number, :student, :study_direction,
+   :advisor_degree, :department_name, :advisor_name, :total_score, :city, :year
+
+    def initialize
+      @faculty = 'Факультет'
+      @work_title = 'Название работы'
+      @course_number = '№ курса'
+      @group_number = '№ группы'
+      @student = 'Ученик'
+      @study_direction = 'Название направления подготовки'
+      @advisor_degree = 'Степень научного руководителя '
+      @department_name = 'Название кафедры'
+      @advisor_name = 'ФИО научного руководителя'
+      @total_score = 'Суммарный балл'
+      @city = 'Город'
+      @year = 'Год'
+    end
+
+    def faculty=(value)
+      @faculty = value
+    end
+
+    def work_title=(value)
+      @work_title = value
+    end
+
+    def course_number=(value)
+      @course_number = value
+    end
+
+    def group_number=(value)
+      @group_number = value
+    end
+
+    def student=(value)
+      @student = value
+    end
+
+    def study_direction=(value)
+      @study_direction = value
+    end
+
+    def advisor_degree=(value)
+      @advisor_degree = value
+    end
+
+    def department_name=(value)
+      @department_name = value
+    end
+
+    def advisor_name=(value)
+      @advisor_name = value
+    end
+
+    def total_score=(value)
+      @total_score = value
+    end
+
+    def city=(value)
+      @city = value
+    end
+
+    def year=(value)
+      @year = value
+    end
+  end
+
   # Your code goes here...
   def self.start
     puts "Please choose option
@@ -17,33 +85,22 @@ module DocxTemplate
   end
 
   def self.replace(filename, name)
-    # replacements = {
-    #   'Факультет': "$A",
-    #   'Название работы': "$B",
-    #   '№ курса': "$C",
-    #   '№ группы': "$D",
-    #   'ученик': "$E",
-    #   'названиенаправленияподготовки': "$F",
-    #   'степеньнаучногоруководителя': "$G",
-    #   'названиекафедры': "$H",
-    #   'ФИОнаучногоруководителя': "$I",
-    #   'Суммарныйбалл': "$J",
-    #   'Город': "$K",
-    #   'Год': "$L"
-    # }
+    replacementsClass = Replacements.new
+    replacementsClass.faculty = 'Факультет страданий'
+
     replacements = {
-      "$1": "Отдел По Борьбе С П@нтами и активистами, а также Симигуками",
-      "$2": "Сформулировать проблему диверсификации инвестиционных рисков",
-      "$3": "666",
-      "$4": "228",
-      "$5": "Абдулхамидов Арамановолваолва Ибналохмат",
-      "$6": "менеджмет",
-      "$7": "ееее",
-      "$8": "кафедра суетологии",
-      "$9": "Абдулхамидов Арамановолваолва Ибналохмат",
-      "#0": "777",
-      "#1": "Дорог",
-      "#2": "2222"
+      "$1": replacementsClass.faculty,
+      "$2": replacementsClass.work_title,
+      "$3": replacementsClass.course_number,
+      "$4": replacementsClass.group_number,
+      "$5": replacementsClass.student,
+      "$6": replacementsClass.study_direction,
+      "$7": replacementsClass.advisor_degree,
+      "$8": replacementsClass.department_name,
+      "$9": replacementsClass.advisor_name,
+      "#0": replacementsClass.total_score,
+      "#1": replacementsClass.city,
+      "#2": replacementsClass.year
     }
 
     doc = Docx::Document.open(filename)
@@ -51,7 +108,7 @@ module DocxTemplate
 
     # read_variable_doc.paragraphs.each do |read_str|
       # for (key, item) in read_Variable
-        
+
       # end
       # replacement_value = read_str.to_s.split("–")
       # replacement_key = replacement_value[0].strip.to_sym
@@ -70,7 +127,7 @@ module DocxTemplate
       end
 
       doc.tables.each do |table|
-        last_row = table.rows.last 
+        last_row = table.rows.last
         last_row.cells.each do |cell|
           cell.paragraphs.each do |paragraph|
             paragraph.each_text_run do |text|
@@ -85,6 +142,7 @@ module DocxTemplate
     # end
 
     doc.save(name)
+    true
   end
 
   def self.create_template(option)
